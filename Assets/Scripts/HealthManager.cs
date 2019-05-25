0b02a65739d4d9f14c5a61d0a0975d3871d4f4aa
Rahul Kukreja <rkuk591@gmail.com>
2019-05-25 12:04:21 +0800

    UN402 project committed

---
+++ Assets/Scripts/HealthManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthManager : MonoBehaviour 
+{
+    public Text livesText;
+    public int lives;
+    public Player myPlayer;
+
+    private void Start()
+    {
+        livesText.text = lives.ToString();
+    }
+
+    public void LoseLife()
+    {
+        lives = lives - 1;
+        livesText.text = lives.ToString();
+        if(lives == 0)
+        {
+            Destroy(myPlayer.gameObject);
+        } 
+    }
+
+
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public float speed = 3.0f;
+    public float jumpForce = 5f;
+    public Rigidbody2D rigidbody;
+    public bool isGrounded;
+    public HealthManager healthManager;
+    // Use this for initialization
+    void Start () {
+	}
+
+	
+	// Update is called once per frame
+	void Update () {
+        if(Input.GetKey(KeyCode.D))
+        {
+            transform.position = transform.position + (Vector3.right * Time.deltaTime * speed);
+        }
+
+        if(Input.GetKey(KeyCode.A))
+        {
+            transform.position = transform.position + (Vector3.left * Time.deltaTime * speed);
+        }
+
+        if(Input.GetKey(KeyCode.Space) && isGrounded)
+        {
+            Debug.LogError("Jumping");
+            rigidbody.AddForce(jumpForce * Vector3.up, ForceMode2D.Impulse);
+        }
+	}
+
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground") 
+        {
+            isGrounded = true;
+        }
+        if(collision.gameObject.tag == "Enemy")
+        {
+            healthManager.LoseLife();
+        }
+    }
+
+    public void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            isGrounded = false;
+        }
+    }
+}
